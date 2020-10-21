2ccb075064ea163ba79d0d32394db9084b38cb72
zulfa iniko falaq <palaktais@gmail.com>
2020-10-21 08:47:41 +0700

    first-commit

---
+++ module_server_side/laravel/database/seeds/UserSeeer.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeeer extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
